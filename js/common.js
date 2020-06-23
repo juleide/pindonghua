8dfec9dcf3bcbfd98e9d8f393eaf54e0a1385151
ROSEMUSE <13562987549@sina.cn>
2020-06-23 22:44:06 +0800

    修改首页选项部分

--- js/common.js
+++ js/common.js
@@ -1,8 +1,12 @@
 $(function () { 
     $('[data-toggle="tooltip"]').tooltip();
     
-    $('#maskbox').on("click", function (e) {
+    $(document).on("click", function (e) {
         $('#navlist').collapse('hide');
+    });
+    $('.header-course_option').on("click", function (event) {
+        $('#courselist').collapse('toggle');
+        event.stopPropagation(); 
     });
 
     $(document).on("click",function(){
