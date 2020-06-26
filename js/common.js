659506f19cfa922ac5aa74c051cdada298132c3a
julei <juleide@qq.com>
2020-06-26 15:18:57 +0800

    添加未登录弹窗提示

--- js/common.js
+++ js/common.js
@@ -12,6 +12,21 @@ $(function () {
     $(document).on("click",function(){
         $('#changeSelect').collapse('hide');
     });
+
+    // 未登录时 点击会员中心 显示模态框
+    $('#vip-btn').on('click', function() {
+        $('#myModal').modal('show');
+    })
+
+    // 未登录时 点击收藏夹 显示模态框
+    $('#collect').on('click', function() {
+        $('#myModal').modal('show');
+    })
+
+    // 点击模态框 确定按钮 跳转登录页
+    $('#close-modal').on('click', function() {
+        window.location.href = 'page/login.html';
+    })
 });
 
 
